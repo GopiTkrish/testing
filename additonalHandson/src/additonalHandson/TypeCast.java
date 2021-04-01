e42ea16e0823e88f374096d1d70892f2b4f91b90
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:46:02 +0530

    Update TypeCast.java

--- additonalHandson/src/additonalHandson/TypeCast.java
+++ additonalHandson/src/additonalHandson/TypeCast.java
@@ -1,5 +1,13 @@
 package additonalHandson;
 
 public class TypeCast {
-
+    public class sample7{  
+    public static void main(String args[])  
+    {  
+        double a=6.2;  
+        int b=5;
+        double c=a+b;  
+        System.out.println("addition :"+c);  
+     }  
+}
 }
