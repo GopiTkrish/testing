98790b7ec90d99a073a5a0d35dacd5e9ec8cab94
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:43:17 +0530

    Update Sum3.java

--- additonalHandson/src/additonalHandson/Sum3.java
+++ additonalHandson/src/additonalHandson/Sum3.java
@@ -1,5 +1,16 @@
 package additonalHandson;
 
 public class Sum3 {
-
+    public static void main(String args[])
+    {
+        int n=123;
+        int sum=0;
+        for(int i=0;i<3;i++)
+        {
+            int p=n%10;
+            sum=(sum*10)+p;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
 }
