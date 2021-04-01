f6a3d85052e43d0feb3159e7d76a9ae67e312633
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:47:39 +0530

    Update SquareOfNumber.java

--- additonalHandson/src/additonalHandson/SquareOfNumber.java
+++ additonalHandson/src/additonalHandson/SquareOfNumber.java
@@ -1,5 +1,10 @@
 package additonalHandson;
 
 public class SquareOfNumber {
-
+    public static void main(String args[])  
+    {  
+        double a=3.9;  
+        double s=a*a;  
+        System.out.println("square :"+s);  
+    } 
 }
