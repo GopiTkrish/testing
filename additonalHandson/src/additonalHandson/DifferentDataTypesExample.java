4062d997fae138715a0746ec08e3ba52904af9fe
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:30:43 +0530

    Update DifferentDataTypesExample.java

--- additonalHandson/src/additonalHandson/DifferentDataTypesExample.java
+++ additonalHandson/src/additonalHandson/DifferentDataTypesExample.java
@@ -1,5 +1,16 @@
 package additonalHandson;
 
 public class DifferentDataTypesExample {
+   public static void main(String[] args) {
+        int a;
+        a = 10;
+        double b;
+        b= 10.5;
+        char s;
+        s = 's';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+    }
 
 }
