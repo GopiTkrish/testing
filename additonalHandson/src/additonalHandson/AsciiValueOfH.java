46055101ff78663c5d61c5ee2493b3e241762e44
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:29:24 +0530

    Update AsciiValueOfH.java

--- additonalHandson/src/additonalHandson/AsciiValueOfH.java
+++ additonalHandson/src/additonalHandson/AsciiValueOfH.java
@@ -1,5 +1,10 @@
 package additonalHandson;
 
 public class AsciiValueOfH {
+  public static void main(String[] args)   
+  {  
+      int s = 'h';
+      System.out.println("The ASCII value of h is: "+s);  
+  }
 
 }
