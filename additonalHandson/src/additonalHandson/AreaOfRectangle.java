93539fff3bcc048962b60fd0cb554dae811176d7
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:27:23 +0530

    Update AreaOfRectangle.java

--- additonalHandson/src/additonalHandson/AreaOfRectangle.java
+++ additonalHandson/src/additonalHandson/AreaOfRectangle.java
@@ -1,5 +1,11 @@
 package additonalHandson;
 
 public class AreaOfRectangle {
-
+  public static void main(String args[])  
+    {  
+          int l=2;  
+          int b=3;  
+          int a=l*b;  
+          System.out.println("Area of rectangle :"+a);  
+     } 
 }
