9e9b79237259d502c08517cf993a31318985691d
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:41:34 +0530

    Update Sum1.java

--- additonalHandson/src/additonalHandson/Sum1.java
+++ additonalHandson/src/additonalHandson/Sum1.java
@@ -1,5 +1,17 @@
 package additonalHandson;
 
 public class Sum1 {
-
+    public static void main(String args[])
+    {
+        int num=1234;
+        int sum=0;
+        int temp=num;
+        for(int i=3;i>=0;i--)
+        {
+            int p=(temp/(10^i))-(sum*10);
+            p=p+2;
+            sum=(sum*10)+p;
+        }
+        System.out.println(sum);
+    }
 }
