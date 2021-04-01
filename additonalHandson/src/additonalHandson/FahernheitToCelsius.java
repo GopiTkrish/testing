0f1b94a50fa8aed70d0b0311f17ca87b757f603b
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:34:36 +0530

    Update FahernheitToCelsius.java

--- additonalHandson/src/additonalHandson/FahernheitToCelsius.java
+++ additonalHandson/src/additonalHandson/FahernheitToCelsius.java
@@ -1,5 +1,11 @@
 package additonalHandson;
 
 public class FahernheitToCelsius {
-
+  public static void main (String args[])  
+    { 
+          float Fahrenheit, Celsius;  
+          Fahrenheit = 43;  
+          Celsius  = ((Fahrenheit-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+Celsius);  
+    }
 }
