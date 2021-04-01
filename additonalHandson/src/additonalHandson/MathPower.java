3a265cf5b2ac500d593877174023942dec8530cc
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:39:10 +0530

    Update MathPower.java

--- additonalHandson/src/additonalHandson/MathPower.java
+++ additonalHandson/src/additonalHandson/MathPower.java
@@ -1,5 +1,14 @@
 package additonalHandson;
 
 public class MathPower {
-
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        double c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=Math.pow(a,b);
+        System.out.println(c);
+    }
 }
