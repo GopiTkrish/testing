5f3636c7a1a3376e22cdc43653c6784475982394
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:46:57 +0530

    Update Product.java

--- additonalHandson/src/additonalHandson/Product.java
+++ additonalHandson/src/additonalHandson/Product.java
@@ -1,5 +1,10 @@
 package additonalHandson;
 
 public class Product {
-
+    public static void main(String args[])  
+    {  
+        double a=8.2;  
+        int b=6; 
+        System.out.println("product :"+a*b);  
+    } 
 }
