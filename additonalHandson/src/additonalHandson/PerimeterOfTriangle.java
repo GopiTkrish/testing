ef611dd2e32f257e0e1cec21c57f061368f71556
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:49:12 +0530

    Update PerimeterOfTriangle.java

--- additonalHandson/src/additonalHandson/PerimeterOfTriangle.java
+++ additonalHandson/src/additonalHandson/PerimeterOfTriangle.java
@@ -1,5 +1,8 @@
 package additonalHandson;
 
 public class PerimeterOfTriangle {
+    int side1=2,side2=3,side3=5,peri;
+    peri=side1+side2+side3;
+    System.out.println("perimeter of triangle is",+peri);
 
 }
