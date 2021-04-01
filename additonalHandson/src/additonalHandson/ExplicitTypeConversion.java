bb98757ea569fa1a018db1f4b0a76caf8f64a02c
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:33:33 +0530

    Update ExplicitTypeConversion.java

--- additonalHandson/src/additonalHandson/ExplicitTypeConversion.java
+++ additonalHandson/src/additonalHandson/ExplicitTypeConversion.java
@@ -1,5 +1,10 @@
 package additonalHandson;
 
 public class ExplicitTypeConversion {
-
+    public static void main(String arg[])
+    {
+        char s='d';
+        s=(char)(s+3);
+        System.out.println(s);
+    }
 }
