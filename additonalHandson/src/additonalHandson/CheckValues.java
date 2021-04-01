f411084be65a48613c21b129753b6745548040b0
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:31:37 +0530

    Update CheckValues.java

--- additonalHandson/src/additonalHandson/CheckValues.java
+++ additonalHandson/src/additonalHandson/CheckValues.java
@@ -1,5 +1,15 @@
 package additonalHandson;
 
 public class CheckValues {
-
+  public static void main(String[] args) {
+	    int a=55,b=70;
+	    if(a<50 && a<b)
+	    {
+	        System.out.println("True");
+	    }
+	    else
+	    {
+	       System.out.println("False");
+	    }
+	}
 }
