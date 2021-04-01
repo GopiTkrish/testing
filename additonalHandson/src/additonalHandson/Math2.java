d4decb8c95e0bf25fe2ebdafe99393bd01c9b496
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:38:20 +0530

    Update Math2.java

--- additonalHandson/src/additonalHandson/Math2.java
+++ additonalHandson/src/additonalHandson/Math2.java
@@ -1,5 +1,13 @@
 package additonalHandson;
 
 public class Math2 {
-
+    public static void main(String args[])
+    {
+        int a=2345;
+        a+=8;
+        a/=3;
+        a%=5;
+        a*=5;
+        System.out.println(a);
+    }
 }
