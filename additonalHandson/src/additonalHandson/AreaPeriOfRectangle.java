31adfd12b87d68bc90ef0bc3e78b914ffe70d607
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:52:04 +0530

    Update AreaPeriOfRectangle.java

--- additonalHandson/src/additonalHandson/AreaPeriOfRectangle.java
+++ additonalHandson/src/additonalHandson/AreaPeriOfRectangle.java
@@ -1,5 +1,10 @@
 package additonalHandson;
 
 public class AreaPeriOfRectangle {
-
+    int length=3;
+    int breadth=6;
+    int areaOfRectange=length*breadth;
+    int permeterOfRectange=2*(length*breadth);
+    System.out.println("Area :"areaOfRectange);
+    System.out.println("Perimeter :"perimeterOfRectange);
 }
