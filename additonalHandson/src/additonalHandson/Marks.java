f2bc86f2a4dcb11b277b1227ad55882387857e11
GopiTkrish <53832101+GopiTkrish@users.noreply.github.com>
2021-04-01 16:36:33 +0530

    Update Marks.java

--- additonalHandson/src/additonalHandson/Marks.java
+++ additonalHandson/src/additonalHandson/Marks.java
@@ -1,5 +1,15 @@
 package additonalHandson;
 
 public class Marks {
-
+    public static void main(String[] args) 
+    {
+        int a,b,c;
+        a=78;
+        b=45;
+        c=62;
+        int total=a+b+c;
+        System.out.println("Total marks :"+total);
+        double per=(total*100/300);
+        System.out.println("percentage is :"+per);
+	}
 }
